2e053c03b9e541a675b7abf2659dd69b706bc538
Ziya Mukhtarov <ziya.mukhtarov@ug.bilkent.edu.tr>
2021-11-25 23:52:04 +0300

    Initial commit

---
+++ src/main/java/tr/com/bilkent/wassapp/WassAppApplication.java
@@ -0,0 +1,13 @@
+package tr.com.bilkent.wassapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WassAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WassAppApplication.class, args);
+	}
+
+}
