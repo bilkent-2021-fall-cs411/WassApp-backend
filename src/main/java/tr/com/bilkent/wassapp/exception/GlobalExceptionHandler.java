5a1106df138e3828f64d2ce603d8c1d92063ad77
Ziya Mukhtarov <ziya.mukhtarov@ug.bilkent.edu.tr>
2021-12-09 20:43:59 +0300

    Improve exception handling
    Change database credentials.

--- src/main/java/tr/com/bilkent/wassapp/exception/GlobalExceptionHandler.java
+++ src/main/java/tr/com/bilkent/wassapp/exception/GlobalExceptionHandler.java
@@ -1,20 +1,63 @@
 package tr.com.bilkent.wassapp.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import tr.com.bilkent.wassapp.model.HTTPResponse;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 @Slf4j
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(RuntimeException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public HTTPResponse<String> handleRuntimeException(RuntimeException e) {
-        log.error(e.getMessage());
-        return new HTTPResponse<>(HttpStatus.BAD_REQUEST.value(), e.getMessage());
+    public HTTPResponse<String> handleRuntimeException(RuntimeException ex) {
+        log.error(ex.getMessage());
+        return new HTTPResponse<>(HttpStatus.BAD_REQUEST.value(), ex.getMessage());
     }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public HTTPResponse<String> handleRuntimeException(Exception ex) {
+        log.error(ex.getMessage());
+        return new HTTPResponse<>(HttpStatus.INTERNAL_SERVER_ERROR.value(), ex.getMessage());
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        log.error("HTTP message not readable exception: {}", ex.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new HTTPResponse<>(HttpStatus.BAD_REQUEST.value(), ex.getMessage()));
+    }
+
+    @Override
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        Map<String, String> errors = new HashMap<>();
+        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+            errors.put(error.getField(), error.getDefaultMessage());
+        }
+        for (ObjectError error : ex.getBindingResult().getGlobalErrors()) {
+            errors.put(error.getObjectName(), error.getDefaultMessage());
+        }
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new HTTPResponse<>(HttpStatus.BAD_REQUEST.value(), "Field validation errors exists", errors));
+    }
+
 }
--- src/main/java/tr/com/bilkent/wassapp/model/HTTPResponse.java
+++ src/main/java/tr/com/bilkent/wassapp/model/HTTPResponse.java
@@ -1,8 +1,10 @@
 package tr.com.bilkent.wassapp.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class HTTPResponse<T> {
 
     private int status;
